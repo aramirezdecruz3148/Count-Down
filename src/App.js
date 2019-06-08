96553ac7ef2f8534028b289f4fc06a72b076ee1e
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-08 08:30:03 -0700

    got my header rendered

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Component from '../utilities/Component.js';
+import Header from './Header.js';
 
 class App extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const main = dom.querySelector('main');
+        
+        const header = new Header();
+        dom.insertBefore(header.render(), main);
+
+        
+
+        return dom;
+    }
+
     renderTemplate() {
         return /*html*/`
         <div>
-            <main>TEST
+            <main>
             </main>
         </div>
         `;
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,9 @@ import Component from '../utilities/Component.js';
 class Header extends Component {
     renderTemplate() {
         return /*html*/`
-        
+        <header>
+            <h1>The Final Count Down!</h1>
+        </header>
         `;
     }
 }
