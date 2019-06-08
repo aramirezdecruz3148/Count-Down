dfc8fcb482b20fe20f5d0c2a53d9d866f0481947
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-08 08:41:49 -0700

    added the javascript logic for the countdown clock

--- src/Clock.js
+++ src/Clock.js
@@ -3,7 +3,28 @@ import Component from '../utilities/Component.js';
 class Clock extends Component {
     render() {
         const dom = this.renderDOM();
-
+        const deadline = new Date('sept 25, 2019 18:01:00').getTime(); 
+  
+        const x = setInterval(function() { 
+            const now = new Date().getTime(); 
+            const t = deadline - now; 
+            const days = Math.floor(t / (1000 * 60 * 60 * 24)); 
+            const hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
+            const minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+            const seconds = Math.floor((t % (1000 * 60)) / 1000); 
+            document.getElementById('day').innerHTML = days ; 
+            document.getElementById('hour').innerHTML = hours; 
+            document.getElementById('minute').innerHTML = minutes;  
+            document.getElementById('second').innerHTML = seconds;  
+            if(t < 0) { 
+                clearInterval(x); 
+                document.getElementById('demo').innerHTML = 'TIME UP'; 
+                document.getElementById('day').innerHTML = '0'; 
+                document.getElementById('hour').innerHTML = '0'; 
+                document.getElementById('minute').innerHTML = '0' ;  
+                document.getElementById('second').innerHTML = '0'; } 
+        }, 1000);
+         
         return dom;
     }
 
