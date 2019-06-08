ac618fc80dfff33695a3b7e0b0163f4af07f9a49
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-08 08:26:08 -0700

    added html-to-DOM.js file, forgot that base Component needed it

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     renderTemplate() {
         return /*html*/`
         <div>
-            <main>
+            <main>TEST
             </main>
         </div>
         `;
