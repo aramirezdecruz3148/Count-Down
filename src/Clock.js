9639875986a969af02afa676c6d4580e6246866e
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-08 08:44:33 -0700

    I renamed some variables

--- src/Clock.js
+++ src/Clock.js
@@ -7,16 +7,16 @@ class Clock extends Component {
   
         const x = setInterval(function() { 
             const now = new Date().getTime(); 
-            const t = deadline - now; 
-            const days = Math.floor(t / (1000 * 60 * 60 * 24)); 
-            const hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
-            const minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
-            const seconds = Math.floor((t % (1000 * 60)) / 1000); 
+            const timeDifference = deadline - now; 
+            const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24)); 
+            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
+            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60)); 
+            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000); 
             document.getElementById('day').innerHTML = days ; 
             document.getElementById('hour').innerHTML = hours; 
             document.getElementById('minute').innerHTML = minutes;  
             document.getElementById('second').innerHTML = seconds;  
-            if(t < 0) { 
+            if(timeDifference < 0) { 
                 clearInterval(x); 
                 document.getElementById('demo').innerHTML = 'TIME UP'; 
                 document.getElementById('day').innerHTML = '0'; 
@@ -24,7 +24,7 @@ class Clock extends Component {
                 document.getElementById('minute').innerHTML = '0' ;  
                 document.getElementById('second').innerHTML = '0'; } 
         }, 1000);
-         
+
         return dom;
     }
 
